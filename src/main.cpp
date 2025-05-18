b45e665683979193c59b9373bb2ae5274bae6437
gwenaelrouault <gwenael.rouault@gmail.com>
2025-05-18 15:23:20 +0200

    init panel

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,61 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <stdio.h>
+#include<string>
+
+void draw_menu_bar() {
+    if (ImGui::BeginMainMenuBar()) {
+        if (ImGui::BeginMenu("Fichier")) {
+            ImGui::MenuItem("Nouveau");
+            ImGui::MenuItem("Ouvrir");
+            ImGui::MenuItem("Quitter");
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+}
+
+void draw_device_screen(double height) {
+    ImGui::BeginChild("SDL Frame", ImVec2(0, height), true);
+    ImGui::Text("Zone de rendu SDL (simulée)");
+    ImGui::Dummy(ImVec2(400, 200));
+    ImGui::EndChild();
+}
+
+void draw_main_panel(ImGuiIO& io) {
+    char text_buffer[4096] = "Écris ton code ici...";
+    ImGui::SetNextWindowPos(ImVec2(0, 20), ImGuiCond_Always);
+    ImGui::SetNextWindowSize(io.DisplaySize, ImGuiCond_Always);
+    ImGui::Begin("Main Window", nullptr,
+        ImGuiWindowFlags_NoTitleBar |
+        ImGuiWindowFlags_NoResize |
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoCollapse);
+
+    ImGui::Columns(2);
+
+    // Colonne de gauche : éditeur de texte
+    ImGui::BeginChild("TextEditor", ImVec2(0, 0), true);
+    ImGui::Text("Éditeur de texte");
+    ImGui::InputTextMultiline("##Text", text_buffer, IM_ARRAYSIZE(text_buffer),
+                                  ImVec2(-FLT_MIN, ImGui::GetContentRegionAvail().y));
+    ImGui::EndChild();
+    ImGui::NextColumn();
+
+    // Colonne de droite : split vertical
+    float childHeight = ImGui::GetContentRegionAvail().y / 2.0f;
+
+    draw_device_screen(childHeight);
+
+    ImGui::BeginChild("ImGui Panel", ImVec2(0, 0), true);
+    ImGui::Text("Panneau ImGui en bas");
+    ImGui::Button("Exécuter");
+    ImGui::SameLine();
+    ImGui::Button("Analyser");
+    ImGui::EndChild();
+
+    ImGui::End();
+}
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER);
@@ -41,16 +96,14 @@ int main() {
                 running = false;
         }
 
-        // Start ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Hello, ImGui");
-        ImGui::Text("SDL + OpenGL3 backend");
-        ImGui::End();
+        draw_menu_bar();
 
-        // Rendering
+        draw_main_panel(io);
+        
         ImGui::Render();
         glViewport(0, 0, 1280, 720);
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
